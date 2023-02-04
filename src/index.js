63471e4a2d72d17c864b7ba653bab13d60d46c13
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-05 00:29:17 +0300

    Add app styling and import to App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 // component file
 import TodoContainer from './components/TodoContainer';
 
+// stylesheet
+import './App.css';
+
 const root = document.getElementById('root');
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
