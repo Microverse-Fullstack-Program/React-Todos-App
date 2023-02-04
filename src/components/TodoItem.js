2abe8cbd935490b3790b16708bd2e96a88de00fd
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-04 19:54:04 +0300

    Add the TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class TodoItem extends React.Component {
-  render() {
-    const { todo } = this.props;
-    return <li>{todo.title}</li>;
-  }
-}
+const TodoItem = (props) => {
+  const { todo, handleChangeProps, deleteTodoProps } = props;
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => handleChangeProps(todo.id)}
+      />
+      {todo.title}
+      <button type="button" onClick={() => deleteTodoProps(todo.id)}>
+        Delete
+      </button>
+    </li>
+  );
+};
 
 export default TodoItem;
