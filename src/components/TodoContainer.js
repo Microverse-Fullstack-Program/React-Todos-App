b4e47635ff308b787b98864bc68c424f1c73f198
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-05 00:27:43 +0300

    Update TodoContainer class-component

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,35 +1,87 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Header from './Header';
 import TodosList from './TodosList';
+import InputTodo from './InputTodo';
+import {
+  FetchLocalStorage,
+  AddToLocalStorage,
+  RemoveFromLocalStorage,
+} from './PersistInLocalStorage';
 
 class TodoContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [
-        {
-          id: 1,
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: 2,
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: 3,
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ],
+      todos: FetchLocalStorage(),
     };
+  }
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    const { todos } = this.state;
+    this.setState({
+      todos: [...todos, newTodo],
+    });
+    AddToLocalStorage(newTodo);
+  };
+
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  deleteTodo = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: [...todos.filter((todo) => todo.id !== id)],
+    });
+    RemoveFromLocalStorage(id);
+  };
+
+  setUpdate = (updatedTitle, id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.map((todo) => {
+        if (todo.id === id && updatedTitle) {
+          return {
+            ...todo,
+            title: updatedTitle,
+          };
+        }
+        return todo;
+      }),
+    });
   }
 
   render() {
     const { todos } = this.state;
+
     return (
-      <div>
-        <TodosList todos={todos} />
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.deleteTodo}
+            setUpdate={this.setUpdate}
+          />
+        </div>
       </div>
     );
   }
