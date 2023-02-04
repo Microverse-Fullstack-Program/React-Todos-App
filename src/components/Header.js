755d108187bf7e51ac361f26c54d787843ffd423
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-04 20:02:00 +0300

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { React, Component } from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>todos</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
