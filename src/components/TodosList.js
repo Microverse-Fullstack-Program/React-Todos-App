9db336d89b9af2cffb4f19a0bfed76d15d268226
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-04 19:54:18 +0300

    Add the TodoList component

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class TodosList extends React.Component {
-  render() {
-    const { todos } = this.props;
-    return (
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
-        ))}
-      </ul>
-    );
-  }
-}
+const TodosList = (props) => {
+  const { todos, handleChangeProps, deleteTodoProps } = props;
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={handleChangeProps}
+          deleteTodoProps={deleteTodoProps}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default TodosList;
