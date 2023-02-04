4dec79a446de521b5842a26634e8ac544e6814bb
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-04 18:08:13 +0300

    Adding State

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class TodoItem extends React.Component {
+  render() {
+    const { todo } = this.props;
+    return <li>{todo.title}</li>;
+  }
+}
+
+export default TodoItem;
