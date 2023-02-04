823883ed68f5df10eb955e12f62231b5bb4c4116
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-05 00:26:19 +0300

    Update TodoItem class-component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,76 @@
 import React from 'react';
+import styles from './TodoItem.module.css';
+import { EditLocalStorage } from './PersistInLocalStorage';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+    };
+  }
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    });
+  };
+
+  handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({ editing: false });
+      const { todo } = this.props;
+      EditLocalStorage(todo);
+    }
+  }
+
   render() {
-    const { todo } = this.props;
+    const {
+      todo, handleChangeProps, deleteTodoProps, setUpdate,
+    } = this.props;
+    const { completed, id, title } = todo;
+    const { editing } = this.state;
+    const viewMode = {};
+    const editMode = {};
+
+    if (editing) {
+      viewMode.display = 'none';
+    } else {
+      editMode.display = 'none';
+    }
+
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+
     return (
-      <li>
-        {todo.title}
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => handleChangeProps(id)}
+          />
+          <button type="button" onClick={() => deleteTodoProps(id)}>
+            Delete
+          </button>
+          <span style={completed ? completedStyle : null}>{title}</span>
+        </div>
         <input
-          type="checkbox"
-          checked={todo.completed}
-          onChange={() => handleChangeProps(todo.id)}
+          type="text"
+          className={styles.textInput}
+          style={editMode}
+          value={title}
+          onChange={(e) => {
+            setUpdate(e.target.value, id);
+          }}
+          onKeyDown={this.handleUpdatedDone}
         />
-        {todo.title}
-        <button type="button" onClick={() => deleteTodoProps(todo.id)}>
-          Delete
-        </button>
       </li>
     );
   }
