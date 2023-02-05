89decd8abd177a98e73ac0bb6ed0c2ab77410bd2
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-05 20:58:40 +0300

    Add router to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 // stylesheet
 import './App.css';
 
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
 // component file
 import TodoContainer from './components/TodoContainer';
+import About from './pages/About';
+import NotMatch from './pages/NotMatch';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <TodoContainer />;
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <TodoContainer />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <NotMatch />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
