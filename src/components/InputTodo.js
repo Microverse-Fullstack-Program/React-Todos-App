3b0dcb83a7dd18612fb2b79f2762b87e6824019f
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-05 13:50:18 +0300

    Convert InputTodo to function component

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,22 +1,79 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const InputTodo = () => {
-  const [state] = useState({
+const InputTodo = (props) => {
+  const [inputText, setInputText] = useState({
     title: '',
   });
 
+  const [errorMsg, setErrorMsg] = useState('');
+  const [border, setBorder] = useState({
+    border: 'none',
+  });
+
+  const { addTodoProps } = props;
+
+  const clearErrorMsg = () => {
+    setErrorMsg('');
+    setBorder({
+      border: 'none',
+    });
+  };
+
   const onChange = (e) => {
-    setState({
-        [e.target.name]: e.target.value
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    });
+    clearErrorMsg();
+  };
+
+  const onBlur = () => clearErrorMsg();
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (inputText.title.trim()) {
+      addTodoProps(inputText.title);
+      setInputText({
+        title: '',
       });
+    } else {
+      setErrorMsg('This is required. Please input todo item.');
+      setBorder({ border: '1px solid red' });
+    }
+  };
+
+  const errorMessage = {
+    display: 'block',
+    color: '#FF0000',
+    fontWeight: 'bold',
+    padding: '1px 1px 15px 25px',
   };
 
   return (
-    <form>
-      <input type="text" placeholder="Add Todo..." value={state.title} onChange={onChange} />
-      <button type="button">Add</button>
-    </form>
+    <>
+      <span style={errorMessage}>{errorMsg}</span>
+      <form onSubmit={handleFormSubmit} onBlur={onBlur} className="form-container">
+        <input
+          className="input-text"
+          style={border}
+          type="text"
+          name="title"
+          placeholder="Add Todo..."
+          required
+          value={inputText.title}
+          onChange={onChange}
+        />
+        <button type="submit" className="input-submit">
+          Submit
+        </button>
+      </form>
+    </>
   );
+};
+
+InputTodo.propTypes = {
+  addTodoProps: PropTypes.func.isRequired,
 };
 
 export default InputTodo;
