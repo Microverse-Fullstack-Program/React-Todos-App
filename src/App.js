386c09aa9f6df6ada97bef3a1ef8bb68064b3c1d
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-06 05:14:55 +0300

    Debug and fix dynamic route error

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import TodoContainer from './components/TodoContainer';
 import About from './pages/About';
+import SinglePage from './pages/SinglePage';
 import NotMatch from './pages/NotMatch';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<TodoContainer />} />
-      <Route path="/about/*" element={<About />} />
+      <Route path="/about" element={<About />}>
+        <Route path=":slug" element={<SinglePage />} />
+      </Route>
       <Route path="*" element={<NotMatch />} />
     </Routes>
   );
